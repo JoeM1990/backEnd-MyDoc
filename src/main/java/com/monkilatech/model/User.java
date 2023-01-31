66485fd63179ef4a88a76091ff10f9c27000e5dd
Joe Monkila <jnkiwa25@gmail.com>
2023-01-31 11:27:20 +0100

    commit

--- src/main/java/com/monkilatech/model/User.java
+++ src/main/java/com/monkilatech/model/User.java
@@ -1,5 +1,29 @@
 package com.monkilatech.model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
 public class User {
 
+    // @Id
+    // @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JsonInclude(Include.NON_EMPTY)
+    private String username;
+
+    @JsonInclude(Include.NON_EMPTY)
+    private String password;
+
+    @JsonInclude(Include.NON_EMPTY)
+    private String phoneNumber;
+
+    @JsonInclude(Include.NON_EMPTY)
+    private String email;
+
+    @JsonInclude(Include.NON_EMPTY)
+    private String photo;
+
+    @JsonInclude(Include.NON_EMPTY)
+    private String typeCompte;
 }
