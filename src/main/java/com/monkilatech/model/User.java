41ce4d79e5bc1f61d8f637f31a29dd859c7b8b98
Joe Monkila <jnkiwa25@gmail.com>
2023-02-03 11:46:28 +0100

    commit

--- src/main/java/com/monkilatech/model/User.java
+++ src/main/java/com/monkilatech/model/User.java
@@ -3,10 +3,23 @@ package com.monkilatech.model;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 
-    // @Id
-    // @GeneratedValue(strategy = GenerationType.IDENTITY)
+     @Id
+     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @JsonInclude(Include.NON_EMPTY)
