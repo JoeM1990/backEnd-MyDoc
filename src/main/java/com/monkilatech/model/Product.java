93563ff6e304e877f32ca8bceddcdf93a4a966ed
Joe Monkila <jnkiwa25@gmail.com>
2023-02-03 11:54:23 +0100

    commit

---
+++ src/main/java/com/monkilatech/model/Product.java
@@ -0,0 +1,31 @@
+package com.monkilatech.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @JsonInclude(Include.NON_EMPTY)
+    private String name;
+
+    @JsonInclude(Include.NON_EMPTY)
+    private String categorie;
+
+    @JsonInclude(Include.NON_EMPTY)
+    private String price;
+
+    @JsonInclude(Include.NON_EMPTY)
+    private String image;
+}
